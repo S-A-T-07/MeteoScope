2906fa544905bda2b44f0a17b0c4415428f87ae6
winter-x64 <itswinterx64@gmail.com>
2025-10-04 22:56:11 +0530

    feat: template to home page

--- app/views/home/land.py
+++ app/views/home/land.py
@@ -1,22 +1,22 @@
-from flask import Blueprint, session
-
-from supabase_client import supabase
+from flask import Blueprint, render_template
 
 home_bp = Blueprint("home", __name__)
 
 
 @home_bp.route("/")
 def index():
-    if "user" in session:
-        # Fetch profile from Supabase
-        profile = (
-            supabase.table("user")
-            .select("name")
-            .eq("user_id", session["user"]["id"])
-            .execute()
-        )
-        name = profile.data[0]["name"] if profile.data else "User"
-        return f"✅ Welcome {name} ({session['user']['email']}) <br><a href='/logout'>Logout</a>"
-    return (
-        "Not logged in <br><a href='/login'>Login</a> | <a href='/signup'>Sign Up</a>"
-    )
+    # if "user" in session:
+    #     # Fetch profile from Supabase
+    #     profile = (
+    #         supabase.table("user")
+    #         .select("name")
+    #         .eq("user_id", session["user"]["id"])
+    #         .execute()
+    #     )
+    #     name = profile.data[0]["name"] if profile.data else "User"
+    #     return f"✅ Welcome {name} ({session['user']['email']}) <br><a href='/logout'>Logout</a>"
+    # return (
+    #     "Not logged in <br><a href='/login'>Login</a> | <a href='/signup'>Sign Up</a>"
+    # )
+
+    return render_template("home/land.html")
