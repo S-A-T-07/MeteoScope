847e8e6c58ad6ea1760fb45377fdc0d45fcf2f53
Soumya Annie Thomas <soumyaannie07@gmail.com>
2025-10-04 15:14:28 +0530

    enhanced_landingpage

---
+++ script.js
@@ -0,0 +1,8 @@
+// A simple script to demonstrate button interaction
+document.addEventListener("DOMContentLoaded", () => {
+  const startButton = document.getElementById("startButton");
+
+  startButton.addEventListener("click", () => {
+    alert("METEOSCOPE is starting! (This is a frontend demo.)");
+  });
+});
