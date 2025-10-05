b8d12c5d19c482da1a8b56956df60a9031f8ff03
winter-x64 <itswinterx64@gmail.com>
2025-10-05 10:24:21 +0530

    fix: events

--- app/views/user/event.py
+++ app/views/user/event.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, render_template, request, session, url_fo
 
 from app.supabase.supabase_client import supabase_client
 
-event_bp = Blueprint("event", __name__, url_prefix="/event")
+event_bp = Blueprint("event", __name__, url_prefix="/user")
 
 
 @event_bp.route("/create_event", methods=["GET", "POST"])
@@ -12,7 +12,18 @@ def create_event():
 
     if request.method == "POST":
         event_name = request.form["event_name"]
+        event_date = request.form["event_date"]
+        event_location = request.form["event_location"]
+        event_select = request.form["event_select"]
         user_id = session["user"]["id"]
+
+        data = {
+            "event_name": event_name,
+            "event_date": event_date,
+            "event_location": event_location,
+            "event_select": event_select,
+            "user_id": user_id,
+        }
 
         try:
             profile = (
